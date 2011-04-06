99410fec8f49a63b82695b325937f8d1707eff73
Brian Ketelsen <bketelsen@gmail.com>
2011-04-06 00:07:09 -0400

    more awesomeness

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/garyburd/twister/web"
+	"github.com/garyburd/twister/server"
+	"template"
+	"os"
+	"log"
+	
+)
+
+func loadPage(path string)(page *Page, err os.Error){
+	//will return a mongodb document
+	log.Println(path)
+	return &Page{Path:path,Title:"Clarity Services Home Page", Content:"Your body goes here"}, nil
+}
+
+func viewHandler(req *web.Request) {
+	path := req.Param.Get("path")
+	p, err := loadPage(path)
+	if err != nil {
+		// do something!
+	}
+
+	renderTemplate(req, "public_base", p)
+	
+}
+
+var templates = make(map[string]*template.Template)
+
+func init() {
+	for _, tmpl := range []string{"public_base"} {
+		templates[tmpl] = template.MustParseFile("templates/"+tmpl+".html", nil)
+	}
+}
+
+func renderTemplate(req *web.Request, tmpl string, p *Page) {
+	err := templates[tmpl].Execute(
+		req.Respond(web.StatusOK),
+		map[string]interface{}{
+			"page": p,
+			"xsrf": req.Param.Get("xsrf"),
+		})
+	if err != nil {
+		log.Println("error rendering", tmpl, err)
+	}
+}
+
+func main() {
+
+	// this is a bad regex, will only handle single word, not full path
+	const pathParam = "<path:.*>"
+
+	h := web.ProcessForm(10000, true, // limit size of form to 10k, enable xsrf
+		web.NewRouter().
+			Register("/static/<path:.*>", "GET", web.DirectoryHandler("static/")).
+			Register("/<path:(.*)>", "GET", viewHandler))
+	server.Run(":8080", h)
+}
+
--- schema.go
+++ schema.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type Page struct {
 	Path    string
 	Title   string
@@ -19,4 +21,42 @@ type PressRelease struct {
 	Date      string
 	Title     string
 	PathToPdf string
+}
+
+func (p *Page) IsMetrics() string {
+	if strings.Contains(p.Path, "metrics"){
+		return "selected"
+	}
+	return ""
+}
+
+func (p *Page) IsContact() string {
+	if strings.Contains(p.Path, "contact"){
+		return "selected"
+	}
+	return ""
+}
+
+func (p *Page) IsSupport() string {
+	if strings.Contains(p.Path, "support"){
+		return "selected"
+	}
+	return ""
+}
+
+func (p *Page) IsAboutPress() string {
+	if strings.Contains(p.Path, "about"){
+		return "selected"
+	}
+	if strings.Contains(p.Path,"press"){
+		return "selected"
+	}
+	return ""
+}
+
+func (p *Page) IsProducts() string {
+	if strings.Contains(p.Path, "products"){
+		return "selected"
+	}
+	return ""
 }
